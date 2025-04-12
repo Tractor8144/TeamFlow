2bb90b73c9d15adf185c31f241c3b73d384871a0
Detergent4316 <maharshi@midasit.in>
2025-04-12 12:28:38 +0530

    Configure django to use postgres and add requirements.txt

--- teamflow/settings.py
+++ teamflow/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = "teamflow.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamflowdb',
+        'USER': 'teamflowuser',
+        'PASSWORD': 'teamflowuser',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

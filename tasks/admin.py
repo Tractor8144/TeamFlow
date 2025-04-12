d73f9f37cb74556fe064ce442d00a24c2168ff9b
Detergent4316 <maharshi@midasit.in>
2025-04-12 13:34:50 +0530

    Create Task model and migrate

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Task, TaskList
 # Register your models here.
+admin.site.register(TaskList)
+admin.site.register(Task)
--- tasks/models.py
+++ tasks/models.py
@@ -4,12 +4,34 @@ from django.conf import settings
 # Create your models here.
 class TaskList(models.Model):
     name = models.CharField(max_length=255)
-    # created_by = models.ForeignKey(
-    #     settings.AUTH_USER_MODEL,
-    #     on_delete=models.CASCADE,
-    #     related_name='taskslist'
-    # )
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    task_list = models.ForeignKey(
+        'TaskList', 
+        related_name='tasks', 
+        on_delete=models.CASCADE
+    )
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True, blank=True,
+        related_name='tasks_assigned'
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.SET_NULL, 
+        null=True, blank=True,
+        related_name='tasks_created'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title 

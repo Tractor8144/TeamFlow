97a12c38843d4211be886f7aeb9dc043f740621c
Detergent4316 <maharshi@midasit.in>
2025-04-12 23:09:50 +0530

    Add ViewSet for models to pass them to REST requests

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Task, TaskList
+from .serializers import TaskSerializer, TaskListSerializer
 # Create your views here.
+
+class TaskListViewSet(viewsets.ModelViewSet):
+    queryset = TaskList.objects.all()
+    serializer_class = TaskListSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer

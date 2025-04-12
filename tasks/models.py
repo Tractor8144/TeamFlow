aa1a9c6fe0535f607bcaa4edfe0c2d039dc7e12b
Detergent4316 <maharshi@midasit.in>
2025-04-12 13:08:42 +0530

    Add TaskList model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class TaskList(models.Model):
+    name = models.CharField(max_length=255)
+    # created_by = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL,
+    #     on_delete=models.CASCADE,
+    #     related_name='taskslist'
+    # )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

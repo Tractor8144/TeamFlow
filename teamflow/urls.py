07c240cd65e0c147c9a575f5ade63105b8d98434
Detergent4316 <maharshi@midasit.in>
2025-04-13 21:59:51 +0530

    Add UserViewSet (readonly) and expose its urls

--- teamflow/urls.py
+++ teamflow/urls.py
@@ -22,5 +22,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('tasks.urls')),
-    path('api/token', obtain_auth_token, name="api_token")
+    path('api/token', obtain_auth_token, name="api_token"),
+    path('api/', include('accounts.urls')),
 ]
